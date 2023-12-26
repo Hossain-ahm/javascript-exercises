35556bec9b28df33c2cde8b5105397aee9e41b4c
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-12-26 19:50:54 +0000

    completed exercise
    completed exercise 5,6,7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    if 
+    (
+    !(Number.isInteger(x) && Number.isInteger(y)) || (x < 0) || (y < 0)
+    ) 
+    {
+        return "ERROR"
+    }
+    if (x > y) {
+        [x, y] = [y , x]
+    }
+    let sum = 0
+    for (var i = x; i <= y; i++) {
+        sum += i
+    }
+
+    return sum
 
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
