7d5b81834d8d270c4a9967d0937eb566ef1c065f
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-12-26 19:08:19 +0000

    exercises complete
    completed exercises 2,3,4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (var i = 1; i < arguments.length; i++) {
+        let value = arguments[i]
+        array = array.filter(item => item !== value); 
+    }
+    return array;
 };
 
 // Do not edit below this line
