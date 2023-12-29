e933cddff96b11b764e031d471b4ed70cde264b2
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-12-29 01:21:31 +0000

    completed exe 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let processedInput = input.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return processedInput.split('').reverse().join('') == processedInput
 };
 
 // Do not edit below this line
