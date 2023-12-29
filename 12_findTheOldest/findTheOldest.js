6051ba29d670eb46267c6ac4d6215d5fd6fb4192
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-12-29 02:22:17 +0000

    completed exe 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a , b) => {
+        if (age(a) > age(b)){
+            return -1
+        }else return 1
+    })
+    return people[0]
 };
+
+function age(person){
+    if (person.yearOfDeath !== undefined){
+        return person.yearOfDeath - person.yearOfBirth
+    }
+    return (new Date().getFullYear()) - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
