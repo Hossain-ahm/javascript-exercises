d7a9347f2c5cadf98c4eebde1c34fd64398cb734
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-12-29 00:27:22 +0000

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,28 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return  (x + y)
 };
 
-const subtract = function() {
-	
+const subtract = function(x , y) {
+	return (x - y)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum,current) => add(sum, current),0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x, y)
 };
 
-const factorial = function() {
+const factorial = function(x) {
+  if (x == 0){
+    return 1
+  }
+  return x * factorial(x - 1)
 	
 };
 
